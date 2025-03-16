702417534fc3e31add5abcff584e16d2c64f7f37
3RaghavK3 <33raghavk33@gmail.com>
2025-03-16 20:02:04 +0530

    feat:50% timer completed

--- clock/app.js
+++ clock/app.js
@@ -2,7 +2,7 @@ const clock=document.getElementById("cl");
 const stopwatch=document.getElementById("sw");
 const timer=document.getElementById("ti")
 const slides=document.getElementById("slides")
-const itemscount=document.querySelectorAll(".slides div").length
+const itemscount=document.querySelectorAll(".slides>div").length
 const start=document.getElementById("start");
 const play=document.getElementById("button");
 
@@ -79,7 +79,94 @@ const hourcol=document.getElementById("hc");
 const minutecol=document.getElementById("mc");
 const secondcol=document.getElementById("sc");
 
+const wrappers=document.querySelectorAll(".wrapper");
 
+
+
+function populate(){
+    for(let i=0;i<1;i++){
+        let x=document.createElement("div");
+        x.classList.add("item");
+        hourcol.appendChild(x);
+        y=x.cloneNode(true);
+        minutecol.appendChild(y);
+        z=y.cloneNode(true);
+        secondcol.appendChild(z);
+    }
+    
+    
+    for(let i=0;i<=23;i++){
+        let x=document.createElement("div");
+        x.classList.add("item");
+        
+        x.innerText=i.toString().padStart(2,"0");
+        hourcol.appendChild(x);
+    }
+    
+    
+    for(let i=0;i<=59;i++){
+        let x=document.createElement("div");
+        x.classList.add("item");
+        x.innerText=i.toString().padStart(2,"0");
+        minutecol.appendChild(x);
+        y=x.cloneNode(true);
+        secondcol.appendChild(y);
+    }
+}
+
+populate();
+
+const timerbutton=document.getElementById("timerbt");
+timerbutton.addEventListener("click",()=>{
+    slides.style.transform=`translateX(-${100/itemscount * (itemscount-1)}%)`
+    const timerstart=document.getElementById("timer-start");
+    timerstart.textContent=timervalue;
+
+    startcountdown=setInterval(() => {
+        let hrs=parseInt(timerstart.textContent.slice(0,2));
+        let mins=parseInt(timerstart.textContent.slice(3,5));
+        let sc=parseInt(timerstart.textContent.slice(6));
+        
+        sc--;
+
+        if(sc<0){
+            sc=59;
+            mins--;
+        }
+        if(mins<0){
+            mins=59;
+            hrs--;
+        }
+        if(hrs==0 && mins==0 && sc==0){
+            clearInterval(startcountdown);
+            return;
+        }
+
+        timerstart.textContent = 
+            `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${sc.toString().padStart(2, '0')}`;
+        
+    }, 1000);
+})
+
+
+
+
+let timervalue="00:00:00";
+console.log(timervalue);
+document.addEventListener("DOMContentLoaded",()=>{
+    wrappers.forEach((wrapper,index)=>{
+        wrapper.addEventListener("scroll",()=>{
+            let rect=wrapper.getBoundingClientRect();
+            let x=rect.left+rect.width/2;
+            let y=rect.top+rect.height/2;
+            let value=document.elementFromPoint(x,y).textContent;
+            if(index===0) timervalue=`${value}${timervalue.slice(2)}`
+            else if(index===1) timervalue=`${timervalue.slice(0,3)}${value}${timervalue.slice(5)}`;
+            else timervalue=`${timervalue.slice(0,6)}${value}`;
+            console.log(timervalue);
+        });
+    });
+});
 
 
 
